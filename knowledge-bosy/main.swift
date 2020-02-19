df908055bdce4ef3668af6197154219c1478070f
Daniel Schäfer <schaefer.dev@gmail.com>
2020-02-19 14:43:28 +0100

    Initial Commit

---
+++ knowledge-bosy/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  knowledge-bosy
+//
+//  Created by Daniel Schäfer on 19.02.20.
+//  Copyright © 2020 Daniel Schäfer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
