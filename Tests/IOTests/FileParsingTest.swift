b065336ec6705d3c5ca7c33b340a2b2de6ee7e2b
Daniel Schäfer <schaefer.dev@gmail.com>
2020-05-07 13:01:30 +0200

    added more complex test for kripke structure reading

--- Tests/IOTests/FileParsingTest.swift
+++ Tests/IOTests/FileParsingTest.swift
@@ -65,7 +65,7 @@ class FileParsingTest: XCTestCase {
         XCTAssertEqual(dotGraph.initial_states.count, 1)
         XCTAssertEqual(dotGraph.get_state(name: "s0")!.propositions.count, 0)
         XCTAssertEqual(dotGraph.get_state(name: "s1")!.propositions.count, 1)
-        XCTAssertEqual(dotGraph.get_state(name: "s1")!.propositions[0]., "go")
+        XCTAssertEqual(dotGraph.get_state(name: "s1")!.propositions[0].description, "go")
         
         // s0 has outgoing transitions to s1 and s0
         XCTAssertEqual(dotGraph.get_state(name: "s0")!.transitions[0].end.name, "s1")
@@ -99,6 +99,75 @@ class FileParsingTest: XCTestCase {
         XCTAssertEqual(dotGraph.get_state(name: "s1")!.transitions[0].condition.dnf[0].literals[0].description, "true")
         XCTAssertEqual(dotGraph.get_state(name: "s1")!.transitions[0].condition.dnf[0].literals.count, 1)
         XCTAssertEqual(dotGraph.get_state(name: "s1")!.transitions[0].condition.dnf.count, 1)
+    }
+    
+    
+    func testDotGraphParsingComplexer() {
+        let automataInfoOpt = FileParser.readAutomataInfoFile(path: "/Users/daniel/uni_repos/repo_masterThesisSpecifications/kbosy_inputs/xcode_tests/test_automata_kripke.kbosy")
+        XCTAssert(automataInfoOpt != nil)
+        let automataInfo = automataInfoOpt!
+        
+        
+        let dotGraphOpt = FileParser.readDotGraphFile(path: "/Users/daniel/uni_repos/repo_masterThesisSpecifications/kbosy_inputs/xcode_tests/test_automata_kripke.gv", info: automataInfo)
+        XCTAssert(dotGraphOpt != nil)
+        let dotGraph = dotGraphOpt!
+        
+        // s0 is only initial state
+        XCTAssertEqual(dotGraph.initial_states[0].name, "s0")
+        XCTAssertEqual(dotGraph.initial_states[0].propositions.count, 2)
+        XCTAssertEqual(dotGraph.initial_states.count, 1)
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.propositions.count, 2)
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.propositions[0].description, "go")
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.propositions[1].description, "stop")
+        
+        XCTAssertEqual(dotGraph.get_state(name: "s1")!.propositions.count, 1)
+        XCTAssertEqual(dotGraph.get_state(name: "s1")!.propositions[0].description, "go")
+        
+        XCTAssertEqual(dotGraph.get_state(name: "s2")!.propositions.count, 0)
+        
+        
+        // s0 has outgoing transitions to s1 and s0
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.transitions[0].end.name, "s1")
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.transitions[1].end.name, "s0")
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.transitions.count, 2)
+        
+        // s1 has only one outgoing transition to s1
+        XCTAssertEqual(dotGraph.get_state(name: "s1")!.transitions[0].end.name, "s2")
+        XCTAssertEqual(dotGraph.get_state(name: "s1")!.transitions[1].end.name, "s1")
+        XCTAssertEqual(dotGraph.get_state(name: "s1")!.transitions.count, 2)
+        
+        // s2 has only one outgoing transition to s2
+        XCTAssertEqual(dotGraph.get_state(name: "s2")!.transitions[0].end.name, "s2")
+        XCTAssertEqual(dotGraph.get_state(name: "s2")!.transitions.count, 1)
+        
+        
+        // test if conditions are parsed correctly
+        // Test condition of s0->s1
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.transitions[0].condition.dnf[0].literals[0].description, "a")
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.transitions[0].condition.dnf[0].literals.count, 1)
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.transitions[0].condition.dnf[1].literals[0].description, "b")
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.transitions[0].condition.dnf[1].literals.count, 1)
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.transitions[0].condition.dnf[2].literals[0].description, "h")
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.transitions[0].condition.dnf[2].literals.count, 1)
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.transitions[0].condition.dnf.count, 3)
+        
+        
+        // Test condition of s0->s0
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.transitions[1].condition.dnf[0].literals[0].description, "¬a")
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.transitions[1].condition.dnf[0].literals[1].description, "¬b")
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.transitions[1].condition.dnf[0].literals[2].description, "¬h")
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.transitions[1].condition.dnf[0].literals.count, 3)
+        XCTAssertEqual(dotGraph.get_state(name: "s0")!.transitions[1].condition.dnf.count, 1)
+        
+
+        // Transitions s1->s1 not tested
+        // Transitions s1->s2 not tested
+        
+        
+        // Test condition of s2->s2
+        XCTAssertEqual(dotGraph.get_state(name: "s2")!.transitions[0].condition.dnf[0].literals[0].description, "true")
+        XCTAssertEqual(dotGraph.get_state(name: "s2")!.transitions[0].condition.dnf[0].literals.count, 1)
+        XCTAssertEqual(dotGraph.get_state(name: "s1")!.transitions[1].condition.dnf.count, 1)
     }
 
 }
