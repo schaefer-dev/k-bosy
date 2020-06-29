2e140ad5926240d00876883bdce1529bca39baf9
Daniel Schäfer <schaefer.dev@gmail.com>
2020-06-29 12:58:16 +0200

    added option to keep tags as inputAPs and add them to state assumptions

--- Sources/KBoSy/main.swift
+++ Sources/KBoSy/main.swift
@@ -29,17 +29,22 @@ do {
                            usage: "A kbosy file which contains LTL spec, assumptions and set of transformation-rules",
                            completion: .filename)
 
-    let automataInfoFile = parser.add(option: "--info", shortName: "-i",
+    let arg_automataInfoFile = parser.add(option: "--info", shortName: "-i",
                                 kind: String.self,
                                 usage: "A Automata file which contains environment automata information",
                                 completion: .filename)
 
-    let dotFile = parser.add(option: "--dot", shortName: "-d",
+    let arg_dotFile = parser.add(option: "--dot", shortName: "-d",
                                 kind: String.self,
                                 usage: "A Dot Graph file which describes the behaviour of the environment. Requires automataInfo to be also given.",
                                 completion: .filename)
 
-    let synthesize = parser.add(option: "--synthesize",
+    let arg_synthesize = parser.add(option: "--synthesize",
+                                kind: Bool.self,
+                                usage: "enables following bosy call to synthesize transformed spec.",
+                                completion: ShellCompletion.none)
+    
+    let arg_tags = parser.add(option: "--tags", shortName: "-t",
                                 kind: Bool.self,
                                 usage: "enables following bosy call to synthesize transformed spec.",
                                 completion: ShellCompletion.none)
@@ -47,23 +52,28 @@ do {
     let argsv = Array(CommandLine.arguments.dropFirst())
     let parguments = try parser.parse(argsv)
 
+    
     /* --------------------------------------------------------------------------------------------- */
     /* Starting of reading Automata file(s) */
-    if let automataInfoFilename = parguments.get(automataInfoFile) {
-        if let dotGraphFilename = parguments.get(dotFile) {
+    if let automataInfoFilename = parguments.get(arg_automataInfoFile) {
+        if let dotGraphFilename = parguments.get(arg_dotFile) {
             
+            var tagsAsAPs = false
+            if let tagsArg = parguments.get(arg_tags), tagsArg {
+                tagsAsAPs = true
+            }
             
-            let spec = LTLSpecBuilder.prepareSynthesis(automataInfoPath: inputFilePrefix + automataInfoFilename, dotGraphPath: inputFilePrefix + dotGraphFilename, tagsAsAPs: false)
+            let spec = LTLSpecBuilder.prepareSynthesis(automataInfoPath: inputFilePrefix + automataInfoFilename, dotGraphPath: inputFilePrefix + dotGraphFilename, tagsAsAPs: tagsAsAPs)
         
 
             let outputFilename = spec.writeJsonToDir(inputFileName: "temp_after_automata_translation", dir: getMasterSpecDirectory())
             print("Output file saved.")
 
-            if let synt = parguments.get(synthesize), synt {
+            if let synt = parguments.get(arg_synthesize), synt {
                   print("\n--------------------------------------------------")
                   print("Calling Bosy now....\n")
                   callBoSy(inputFilename: outputFilename)
-              }
+            }
 
             exit(EXIT_SUCCESS)
         }
