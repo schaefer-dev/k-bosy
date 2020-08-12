226d43880a109962dbabcc34fd07970e6ba009da
Daniel Schaefer <schaefer.dev@gmail.com>
2020-08-12 19:23:06 +0200

    linux build script

---
+++ kbosy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+swift run -c release KBoSyExec ${@:1}
+
+exit_code=$?
+
+# Terminate all tools that may have been started by BoSy
+for f in Tools/*; do
+    if [ ! -f $f ]; then
+        continue
+    fi
+    tool=$(basename $f)
+    killall $tool &> /dev/null
+done
+
+exit $?
