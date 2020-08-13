00133426d88aecc245000801fb0b960f73e76231
Daniel Schaefer <schaefer.dev@gmail.com>
2020-08-13 14:54:37 +0200

    linux timing behaviour fixed

--- Package.swift
+++ Package.swift
@@ -16,8 +16,6 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.5.0")
-        .package(url: "https://github.com/malcommac/SwiftDate.git", from: "5.0.0")
-        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
